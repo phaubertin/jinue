a2fdf0fc2ca812452c3ebec2dd45ea913a8cde5f
Philippe Aubertin <philippe.aubertin@gmail.com>
2009-03-22 17:36:50 +0000

    avancement vm et slab
    git-svn-id: svn+ssh://treplev/dev/jinue@13 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

--- include/vm.h
+++ include/vm.h
@@ -4,80 +4,110 @@
 #include <kernel.h>
 #include <jinue/vm.h>
 
+/* ------ page tables ------ */
+
+/* type of a page table (or page directory) entry */
+typedef unsigned long pte_t;
+
 /* number of bits in virtual address for page table entry */
-#define PT_BITS 10
+#define PAGE_TABLE_BITS 10
 
 /* number of entries in page table */
-#define PT_ENTRIES (1<<PT_BITS)
+#define PAGE_TABLE_ENTRIES (1<<PAGE_TABLE_BITS)
 
 /* bit mask for page table entry */
-#define PT_MASK (PT_ENTRIES - 1)
+#define PAGE_TABLE_MASK (PAGE_TABLE_ENTRIES - 1)
 
 /* size of a page table */
-#define PT_SIZE PAGE_SIZE
+#define PAGE_TABLE_SIZE PAGE_SIZE
 
 /* size of a page table entry, in bytes */
 #define PTE_SIZE 4
 
-/* number of bits in virtual address for page directory entry */
-#define PD_BITS 10
-
-/* number of entries in page directory */
-#define PD_ENTRIES (1<<PD_BITS)
-
-/* bit mask for page directory entry */
-#define PD_MASK (PD_ENTRIES - 1)
-
-/* size of page directory */
-#define PD_SIZE PAGE_SIZE
-
-/* size of a page directory entry, in bytes */
-#define PDE_SIZE 4
-
 /* page table entry of virtual address */
-#define PT_OFFSET_OF(x)  ( ((unsigned long)(x) >> PAGE_BITS) & PT_MASK )
+#define PAGE_TABLE_OFFSET_OF(x)  ( ((unsigned long)(x) >> PAGE_BITS) & PAGE_TABLE_MASK )
 
 /* page directory entry of virtual address */
-#define PD_OFFSET_OF(x)  ( ((unsigned long)(x) >> (PAGE_BITS + PT_BITS)) & PD_MASK )
+#define PAGE_DIRECTORY_OFFSET_OF(x)  ((unsigned long)(x) >> (PAGE_BITS + PAGE_TABLE_BITS))
+
+/* type of a page table */
+typedef pte_t page_table_t[PAGE_TABLE_ENTRIES];
+
+
+/* ------ virtual memory layout ------ */
 
 /* This is where the page tables are mapped in every address space. This
    requires a virtual memory region of size 4M, which must reside completely
-   inside region spanning from KLIMIT to PLIMIT. */
-#define PAGE_TABLES KLIMIT
+   inside region spanning from KLIMIT to PLIMIT. Must be aligned on a 4M
+   boundary */
+#define PAGE_TABLES_MAPPING KLIMIT
 
 /* This is where the page directory is mapped in every address space. It must
    reside in region spanning from KLIMIT to PLIMIT. */
-#define PAGE_DIRECTORY (KLIMIT + PD_ENTRIES * PT_SIZE)
+#define PAGE_DIRECTORY_MAPPING (KLIMIT + PAGE_TABLE_ENTRIES * PAGE_TABLE_SIZE)
 
 /* limits of region spanning from KLIMIT to PLIMIT actually available for
    mappings */
-#define PMAPPING_START (PAGE_DIRECTORY + PD_SIZE)
+#define PMAPPING_START (PAGE_DIRECTORY + PAGE_TABLE_SIZE)
 #define PMAPPING_END   PLIMIT
 
-/* address of page directory entry */
-#define PDE_OF(x) ( (pte_t *)(PAGE_DIRECTORY + PDE_SIZE * PD_OFFSET_OF(x)) )
 
-/* address of page table entry */
-#define PTE_OF(x) ( (pte_t *)(PAGE_TABLES + PT_SIZE * PD_OFFSET_OF(x) + PTE_SIZE * PT_OFFSET_OF(x)) )
+/* ------ mapping of page tables in virtual memory ------ */
 
-/* page table entry for mapping a page table in virtual memory */
-#define PTE_OF_PAGE_TABLE_OF(x) ( (pte_t *)(PAGE_TABLES + PT_SIZE * PD_OFFSET_OF(PAGE_TABLES) + PTE_SIZE * PD_OFFSET_OF(x)) )
+/* page directory in virtual memory */
+#define PAGE_DIRECTORY ( (pte_t *)PAGE_DIRECTORY_MAPPING )
 
-/* flags */
-#define VM_FLAG_KERNEL        0
+/* page tables in virtual memory */
+#define PAGE_TABLES ( (page_table_t *)PAGE_TABLES_MAPPING )
+
+/* page table in virtual memory */
+#define PAGE_TABLE_OF(x) ( PAGE_TABLES[ PAGE_DIRECTORY_OFFSET_OF(x) ] )
+
+/* address of page directory entry in virtual memory */
+#define PDE_OF(x) ( &PAGE_DIRECTORY[ PAGE_DIRECTORY_OFFSET_OF(x) ] )
+
+/* address of page table entry in virtual memory */
+#define PTE_OF(x) ( &PAGE_TABLE_OF(x)[ PAGE_TABLE_OFFSET_OF(x) ] )
+
+/* page table which maps all page tables in memory */
+#define PAGE_TABLES_TABLE ( PAGE_TABLE_OF( PAGE_TABLES_MAPPING ) )
+
+/* address of page entry in PAGE_OF_PAGE_TABLES */
+#define PAGE_TABLE_PTE_OF(x) ( &PAGE_TABLES_TABLE[ PAGE_DIRECTORY_OFFSET_OF(x) ] )
+
+
+/* ------ flags for page attributes ------ */
+
+/* page is present in memory */
 #define VM_FLAG_PRESENT       (1<< 0)
+
+/* page is read only */
 #define VM_FLAG_READ_ONLY     (1<< 1)
+
+/* kernel mode page (default) */
+#define VM_FLAG_KERNEL        0
+
+/* user mode page */
 #define VM_FLAG_USER          (1<< 2)
+
+/* write-through cache policy for page */
 #define VM_FLAG_WRITE_THROUGH (1<< 3)
+
+/* uncached page */
 #define VM_FLAG_CACHE_DISABLE (1<< 4)
+
+/* page was accessed (read) */
 #define VM_FLAG_ACCESSED      (1<< 5)
+
+/* page was written to */
 #define VM_FLAG_DIRTY         (1<< 6)
+
+/* page directory entry describes a 4M page */
 #define VM_FLAG_BIG_PAGE      (1<< 7)
+
+/* page is global (mapped in every address space) */
 #define VM_FLAG_GLOBAL        (1<< 8)
 
-
-typedef unsigned long pte_t;
-typedef pte_t page_table_t[PT_ENTRIES];
 
 void vm_map(addr_t vaddr, addr_t paddr, unsigned long flags);
 void vm_unmap(addr_t addr);
--- kernel/vm.c
+++ kernel/vm.c
@@ -1,9 +1,10 @@
+#include <kernel.h>
 #include <alloc.h>
 #include <vm.h>
 
 void vm_map(addr_t vaddr, addr_t paddr, unsigned long flags) {
 	pte_t *pte;
-	page_table_t *page_table;
+	addr_t page_table;
 	int idx;
 	
 	pte = PDE_OF(vaddr);
@@ -11,18 +12,21 @@ void vm_map(addr_t vaddr, addr_t paddr, unsigned long flags) {
 	/* check if page table must be created */
 	if( !(*pte & VM_FLAG_PRESENT) ) {
 		/* allocate a page for page table */
-		page_table = (page_table_t *)alloc(PAGE_SIZE);
+		page_table = alloc(PAGE_SIZE);
 		
 		/* link to page table from page directory */
 		*pte = (pte_t)page_table | VM_FLAG_PRESENT;
 		
 		/* map page table in the region of memory reserved for that purpose */
-		pte = PTE_OF_PAGE_TABLE_OF(vaddr);
+		pte = PAGE_TABLE_PTE_OF(vaddr);
 		*pte = (pte_t)page_table | VM_FLAG_PRESENT;
 		
+		/* obtain virtual address of new page table */
+		pte = PAGE_TABLE_OF(vaddr);
+		
 		/* zero content of page table */
-		for(idx = 0; idx < PT_ENTRIES; ++idx) {
-			(*page_table)[idx] = 0;
+		for(idx = 0; idx < PAGE_TABLE_ENTRIES; ++idx) {
+			pte[idx] = 0;
 		}
 	}
 	
