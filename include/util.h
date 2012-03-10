4f529c0dc226969a923adc4f04fe3ad698fe9d61
Philippe Aubertin <philippe.aubertin@gmail.com>
2012-03-10 16:19:07 +0000

    changed return type unsigned int -> addr_t for ALIGN_START and ALIGN_END macros
    git-svn-id: svn+ssh://treplev/dev/jinue@60 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

--- include/util.h
+++ include/util.h
@@ -1,10 +1,12 @@
 #ifndef _JINUE_KERNEL_UTIL_H_
 #define _JINUE_KERNEL_UTIL_H_
 
+#include <jinue/types.h>
+
 #define OFFSET_OF(x, s)		( (unsigned int)(x) & ((s)-1) )
 
-#define ALIGN_START(x, s)	( (unsigned int)(x) & ~((s)-1) )
+#define ALIGN_START(x, s)	((addr_t)( (unsigned int)(x) & ~((s)-1) ))
 
-#define ALIGN_END(x, s)	( OFFSET_OF(x, s) == 0 ? (unsigned int)(x) : ALIGN_START(x, s) + s  )
+#define ALIGN_END(x, s)	    ( OFFSET_OF(x, s) == 0 ? (addr_t)(x) : ALIGN_START(x, s) + s  )
 
 #endif
