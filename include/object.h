6416524c9e9ff313e1b66d6c262c056d21afe602
Philippe Aubertin <philippe.aubertin@gmail.com>
2012-03-10 16:21:30 +0000

    add object.h (incomplete)
    git-svn-id: svn+ssh://treplev/dev/jinue@62 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

---
+++ include/object.h
@@ -0,0 +1,23 @@
+#ifndef _JINUE_KERNEL_OBJECT_H
+#define _JINUE_KERNEL_OBJECT_H
+
+
+#define OBJECT_TYPE_SYSCALL			0
+
+#define OBJECT_TYPE_IPC				1
+
+#define OBJECT_TYPE_PROCESS			2
+
+#define OBJECT_TYPE_THREAD			3
+
+
+#define OBJECT_FLAG_CLOSED			(1<<0)
+
+
+typedef struct {
+	unsigned int type;
+	unsigned int flags;
+	unsigned int ref_count;
+} object_header_t;
+
+#endif
