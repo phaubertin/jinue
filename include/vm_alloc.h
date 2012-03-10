8bb1f8332685f2bbdf7c4c8120f7dd422c260c67
Philippe Aubertin <philippe.aubertin@gmail.com>
2012-03-10 16:20:30 +0000

    add vm_alloc.c/h (untested, needs review)
    git-svn-id: svn+ssh://treplev/dev/jinue@61 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

---
+++ include/vm_alloc.h
@@ -0,0 +1,118 @@
+#ifndef _JINUE_KERNEL_VM_ALLOC_H_
+#define _JINUE_KERNEL_VM_ALLOC_H_
+
+#include <jinue/types.h>
+#include <jinue/vm.h>
+#include <stddef.h>
+
+
+#define VM_ALLOC_STACK_ENTRIES	1024
+
+#define VM_ALLOC_BLOCK_SIZE		(VM_ALLOC_STACK_ENTRIES * PAGE_SIZE)
+
+#define VM_ALLOC_BLOCK_MASK		(VM_ALLOC_BLOCK_SIZE - 1)
+
+
+#define VM_ALLOC_WAS_FREE		false
+
+#define VM_ALLOC_WAS_USED		true
+
+
+#define VM_ALLOC_IS_FREE(b)		( (b)->next != NULL && (b)->stack_ptr == NULL )
+
+#define VM_ALLOC_IS_PARTIAL(b)	( (b)->next != NULL && (b)->stack_ptr != NULL )
+
+#define VM_ALLOC_IS_USED(b)		( (b)->next == NULL )
+
+
+#define VM_ALLOC_EMPTY_STACK(b)	( (b)->stack_ptr >= (b)->stack_addr + VM_ALLOC_STACK_ENTRIES )
+
+#define VM_ALLOC_FULL_STACK(b)	( (b)->stack_ptr <= (b)->stack_addr + 1)
+
+#define VM_ALLOC_CANNOT_GROW(b)	( (b)->stack_next >= (b)->base_addr + VM_ALLOC_BLOCK_SIZE )
+
+
+struct vm_alloc_t {
+	/** base address of memory managed by allocator */
+	addr_t base_addr;
+	
+	/** start address of memory actually available to the allocator */
+	addr_t start_addr;
+	
+	/** end address of memory actually available to the allocator */
+	addr_t end_addr;
+
+	/** number of memory blocks managed by this allocator */
+	unsigned int block_count;
+	
+	/** array of memory block descriptors */
+	struct vm_block_t *block_array;
+		
+	/** number of pages allocated for block array */
+	unsigned int array_pages;
+	
+	/** list of completely free blocks */
+	struct vm_block_t *free_list;
+	
+	/** list of partially free blocks */
+	struct vm_block_t *partial_list;
+};
+
+struct vm_block_t {
+	/** base address of memory block */
+	addr_t base_addr;
+	
+	/** allocator to which this block belongs */
+	struct vm_alloc_t *allocator;
+	
+	/** stack pointer for stack of free pages in partially allocated blocks */
+	addr_t *stack_ptr;
+	
+	/** base address of free page stack */
+	addr_t *stack_addr;
+	
+	/** next page address to add to stack, used for deferred stack initialization */
+	addr_t stack_next;
+	
+	/** link previous block in free list */
+	struct vm_block_t *prev;
+	
+	/** link next block in free list */
+	struct vm_block_t *next;
+};
+
+
+typedef struct vm_alloc_t vm_alloc_t;
+
+typedef struct vm_block_t vm_block_t;
+
+
+addr_t vm_alloc(vm_alloc_t *allocator);
+
+addr_t vm_alloc_low_latency(vm_alloc_t *allocator);
+
+void vm_free(vm_alloc_t *allocator, addr_t page);
+
+void vm_alloc_init(vm_alloc_t *allocator, addr_t start_addr, size_t size);
+
+void vm_alloc_init_piecewise(vm_alloc_t *allocator, addr_t start_addr, size_t size, size_t full_size);
+
+void vm_alloc_destroy(vm_alloc_t *allocator);
+
+void vm_alloc_init_allocator(vm_alloc_t *allocator, addr_t start_addr, size_t size);
+
+void vm_alloc_add_region(vm_alloc_t *allocator, addr_t start_addr, addr_t end_addr);
+
+void vm_alloc_free_block(vm_block_t *block);
+
+void vm_alloc_partial_block(vm_block_t *block);
+
+void vm_alloc_custom_block(vm_block_t *block, addr_t start_addr, addr_t end_addr);
+
+void vm_alloc_unlink_block(vm_block_t *block);
+
+void vm_alloc_grow_stack(vm_block_t *block);
+
+addr_t vm_alloc_grow_single(vm_block_t *block);
+
+#endif
