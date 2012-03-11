420766c0d126b338328cead9d5074476e2a0e1c5
Philippe Aubertin <philippe.aubertin@gmail.com>
2012-03-11 23:52:33 +0000

    panic in red
    git-svn-id: svn+ssh://treplev/dev/jinue@64 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

--- kernel/panic.c
+++ kernel/panic.c
@@ -1,8 +1,18 @@
-#include <startup.h>
+#include <debug.h>
 #include <printk.h>
+#include <startup.h>
+#include <vga.h>
 
-void panic(const char *message) {
+
+void panic(const char *message) {	
+	unsigned int color;
+	
+	color = vga_get_color();
+	vga_set_color(VGA_COLOR_RED);
+	
 	printk("KERNEL PANIC: %s\n", message);
+	
+	vga_set_color(color);
+	
 	halt();
 }
-
