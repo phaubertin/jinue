08a86e2f1df38b561d64f1f836d917d2e727a91d
Philippe Aubertin <philippe.aubertin@gmail.com>
2012-03-12 00:03:49 +0000

    fix error in stdarg.h (last commit incomplete/broken, use this one instead)
    git-svn-id: svn+ssh://treplev/dev/jinue@66 c4e893d5-6c35-458f-8eca-9fa6f7f6653b

--- include/kstdc/stdarg.h
+++ include/kstdc/stdarg.h
@@ -3,7 +3,7 @@
 
 typedef	__builtin_va_list	va_list;
 
-#define va_start(ap, parmN)	__builtin_stdarg_start((ap), (parmN))
+#define va_start(ap, parmN)		__builtin_va_start((ap), (parmN))
 #define va_arg					__builtin_va_arg
 #define va_end					__builtin_va_end
 #define va_copy(dest, src)		__builtin_va_copy((dest), (src))
