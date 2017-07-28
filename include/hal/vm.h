d2bb4f0c048a2c9fc055d994d95211a32c853535
Philippe Aubertin <philippe.aubertin@gmail.com>
2017-07-27 22:32:36 -0400

    Merge hal/vm_x86.c,h into hal/vm.c,h

--- include/hal/vm.h
+++ include/hal/vm.h
@@ -48,6 +48,27 @@ extern pte_t *global_page_tables;
 
 extern addr_space_t initial_addr_space;
 
+extern size_t page_table_entries;
+
+/** page table entry offset of virtual (linear) address */
+extern unsigned int (*page_table_offset_of)(addr_t);
+
+extern unsigned int (*page_directory_offset_of)(addr_t);
+
+extern pte_t *(*get_pte_with_offset)(pte_t *, unsigned int);
+
+extern void (*set_pte)(pte_t *, pfaddr_t, int);
+
+extern void (*set_pte_flags)(pte_t *, int);
+
+extern int (*get_pte_flags)(pte_t *);
+
+extern pfaddr_t (*get_pte_pfaddr)(pte_t *);
+
+extern void (*clear_pte)(pte_t *);
+
+extern void (*copy_pte)(pte_t *, pte_t *);
+
 
 void vm_boot_init(void);
 
