f008b82e3944746643e2e0a47d2477477973aeea
Philippe Aubertin <philippe.aubertin@gmail.com>
2022-10-13 23:09:00 -0400

    2022

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Philippe Aubertin.
+ * Copyright (C) 2019-2022 Philippe Aubertin.
  * All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
