981e768844cfb7bf32674759fe6e6371d89a80df
Philippe Aubertin <philippe.aubertin@gmail.com>
2024-11-09 21:23:20 -0500

    Fix typo

--- userspace/lib/libc/descriptors.h
+++ userspace/lib/libc/descriptors.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Philippe Aubertin.
+ * Copyright (C) 2024 Philippe Aubertin.
  * All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
