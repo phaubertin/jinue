87a6c65aba4005a238887b0116ca9f68bdec78d9
Philippe Aubertin <39178965+phaubertin@users.noreply.github.com>
2024-11-19 02:30:47 -0500

    Refactoring: thread_await() (#87)
    Refactor ` thread_await()` into a separate function.

--- include/kernel/domain/entities/thread.h
+++ include/kernel/domain/entities/thread.h
@@ -50,6 +50,8 @@ void thread_run_first(thread_t *thread);
 
 void thread_run(thread_t *thread);
 
+void thread_yield_current(void);
+
 void thread_terminate_current(void);
 
 void thread_switch_to(thread_t *to);
@@ -58,7 +60,7 @@ void thread_switch_to_and_block(thread_t *to);
 
 void thread_block_current_and_unlock(spinlock_t *lock);
 
-void thread_yield_current(void);
+int thread_await(thread_t *thread);
 
 void thread_set_local_storage(thread_t *thread, addr_t addr, size_t size);
 
