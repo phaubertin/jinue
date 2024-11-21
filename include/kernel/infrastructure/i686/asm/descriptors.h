phaubertin/jinue

commit 13ce3249888755d1570ba27a670b14b6f4b6efdb

Reorganize and cleanup architecture-specific code (#83)
* Organize architecture-specific code into subdirectories.
* Split per CPU data access and CPU feature detection into separate
files.
* Rename functions related to page mapping.
* Reorganize architecture-specific definitions in header files.
* Ensure object and dependency files don't conflict when assembly and C
source files have the same name (e.g. thread.c vs thread.asm).

include/kernel/infrastructure/i686/types.h
@@ -110,17 +110,17 @@ typedef struct {
     uint16_t    iomap;
 } tss_t;
 
-struct cpu_data_t {
+struct percpu_t {
+    struct percpu_t     *self;
+    addr_space_t        *current_addr_space;
     seg_descriptor_t     gdt[GDT_LENGTH];
     /* The assembly-language system call entry point for the SYSCALL instruction
      * (fast_amd_entry in trap.asm) makes assumptions regarding the location of
      * the TSS within this structure. */
     tss_t                tss;
-    struct cpu_data_t   *self;
-    addr_space_t        *current_addr_space;
 };
 
-typedef struct cpu_data_t cpu_data_t;
+typedef struct percpu_t percpu_t;
 
 typedef struct {
     uint32_t    edi;

kernel/infrastructure/i686/init.c
@@ -34,18 +34,20 @@
 #include <kernel/domain/services/cmdline.h>
 #include <kernel/domain/services/logging.h>
 #include <kernel/domain/services/panic.h>
+#include <kernel/infrastructure/i686/asm/msr.h>
+#include <kernel/infrastructure/i686/drivers/pic8259.h>
+#include <kernel/infrastructure/i686/drivers/uart16550a.h>
+#include <kernel/infrastructure/i686/drivers/vga.h>
+#include <kernel/infrastructure/i686/isa/instrs.h>
+#include <kernel/infrastructure/i686/isa/regs.h>
+#include <kernel/infrastructure/i686/pmap/init.h>
+#include <kernel/infrastructure/i686/pmap/pae.h>
+#include <kernel/infrastructure/i686/pmap/pmap.h>
 #include <kernel/infrastructure/i686/boot_alloc.h>
-#include <kernel/infrastructure/i686/cpu.h>
-#include <kernel/infrastructure/i686/cpu_data.h>
+#include <kernel/infrastructure/i686/cpuinfo.h>
 #include <kernel/infrastructure/i686/descriptors.h>
 #include <kernel/infrastructure/i686/memory.h>
-#include <kernel/infrastructure/i686/pic8259.h>
-#include <kernel/infrastructure/i686/remap.h>
-#include <kernel/infrastructure/i686/uart16550a.h>
-#include <kernel/infrastructure/i686/vga.h>
-#include <kernel/infrastructure/i686/vm.h>
-#include <kernel/infrastructure/i686/vm_pae.h>
-#include <kernel/infrastructure/i686/x86.h>
+#include <kernel/infrastructure/i686/percpu.h>
 #include <kernel/infrastructure/elf.h>
 #include <kernel/interface/i686/boot.h>
 #include <kernel/interface/i686/interrupt.h>
@@ -87,7 +89,7 @@ static void move_kernel_at_16mb(const bootinfo_t *bootinfo) {
             (addr_t)bootinfo->page_table_klimit,
             (uint32_t)bootinfo->page_directory);
 
-    vm_write_protect_kernel_image(bootinfo);
+    pmap_write_protect_kernel_image(bootinfo);
 }
 
 static bool maybe_enable_pae(
@@ -97,7 +99,7 @@ static bool maybe_enable_pae(
 
     bool use_pae;
 
-    if(cpu_has_feature(CPU_FEATURE_PAE)) {
+    if(cpu_has_feature(CPUINFO_FEATURE_PAE)) {
         use_pae = (config->machine.pae != CONFIG_PAE_DISABLE);
     }
     else {
@@ -110,11 +112,11 @@ static bool maybe_enable_pae(
 
     if(! use_pae) {
         warning("Warning: Physical Address Extension (PAE) not enabled. NX protection disabled.");
-        vm_set_no_pae();
+        pmap_set_no_pae();
     }
     else {
         info("Enabling Physical Address Extension (PAE).");
-        vm_pae_enable(boot_alloc, bootinfo);
+        pae_enable(boot_alloc, bootinfo);
     }
 
     return use_pae;
@@ -145,7 +147,7 @@ static void init_idt(void) {
     }
 }
 
-static void load_selectors(cpu_data_t *cpu_data, boot_alloc_t *boot_alloc) {
+static void load_selectors(percpu_t *cpu_data, boot_alloc_t *boot_alloc) {
     /* Pseudo-descriptor allocation is temporary for the duration of this
      * function only. Remember heap pointer on entry so we can free the
      * pseudo-allocator when we are done. */
@@ -191,7 +193,7 @@ static void remap_text_video_memory(boot_alloc_t *boot_alloc) {
     void *buffer = boot_page_alloc_n(boot_alloc, num_pages);
     void *mapped = (void *)PHYS_TO_VIRT_AT_16MB(buffer);
 
-    vm_boot_map(mapped, VGA_TEXT_VID_BASE, num_pages);
+    pmap_boot_map(mapped, VGA_TEXT_VID_BASE, num_pages);
 
     info("Remapping text video memory at %#p", mapped);
 
@@ -203,7 +205,7 @@ static void remap_text_video_memory(boot_alloc_t *boot_alloc) {
 }
 
 static void enable_global_pages(void) {
-    if(cpu_has_feature(CPU_FEATURE_PGE)) {
+    if(cpu_has_feature(CPUINFO_FEATURE_PGE)) {
         set_cr4(get_cr4() | X86_CR4_PGE);
     }
 }
@@ -219,7 +221,7 @@ static void initialize_page_allocator(boot_alloc_t *boot_alloc) {
 }
 
 static void select_syscall_implementation(void) {
-    if(cpu_has_feature(CPU_FEATURE_SYSCALL)) {
+    if(cpu_has_feature(CPUINFO_FEATURE_SYSCALL)) {
         uint64_t msrval;
 
         syscall_implementation = JINUE_I686_HOWSYSCALL_FAST_AMD;
@@ -234,7 +236,7 @@ static void select_syscall_implementation(void) {
 
         wrmsr(MSR_STAR, msrval);
     }
-    else if(cpu_has_feature(CPU_FEATURE_SYSENTER)) {
+    else if(cpu_has_feature(CPUINFO_FEATURE_SYSENTER)) {
         syscall_implementation = JINUE_I686_HOWSYSCALL_FAST_INTEL;
 
         wrmsr(MSR_IA32_SYSENTER_CS,  SEG_SELECTOR(GDT_KERNEL_CODE, RPL_KERNEL));
@@ -310,7 +312,7 @@ void machine_init(const config_t *config) {
 
     const bootinfo_t *bootinfo = get_bootinfo();
 
-    cpu_detect_features();
+    detect_cpu_features();
 
     check_data_segment(bootinfo);
 
@@ -338,17 +340,13 @@ void machine_init(const config_t *config) {
      * 
      * We need to ensure that the Task State Segment (TSS) contained in this
      * memory block does not cross a page boundary. */
-    assert(sizeof(cpu_data_t) < CPU_DATA_ALIGNMENT);
-    cpu_data_t *cpu_data = boot_heap_alloc(&boot_alloc, cpu_data_t, CPU_DATA_ALIGNMENT);
+    assert(sizeof(percpu_t) < PERCPU_DATA_ALIGNMENT);
+    percpu_t *cpu_data = boot_heap_alloc(&boot_alloc, percpu_t, PERCPU_DATA_ALIGNMENT);
     
     /* initialize per-CPU data */
-    cpu_init_data(cpu_data);
+    init_percpu_data(cpu_data);
     
-    /* Initialize interrupt descriptor table (IDT)
-     *
-     * This function modifies the IDT in-place (see trap.asm). This must be
-     * done before vm_boot_init() because the page protection bits set up by
-     * vm_boot_init() prevent this. */
+    /* Initialize interrupt descriptor table (IDT) */
     init_idt();
 
     /* load segment selectors */
@@ -360,18 +358,18 @@ void machine_init(const config_t *config) {
     exec_file_t kernel;
     get_kernel_exec_file(&kernel, bootinfo);
 
-    addr_space_t *addr_space = vm_create_initial_addr_space(&kernel, &boot_alloc, bootinfo);
+    addr_space_t *addr_space = pmap_create_initial_addr_space(&kernel, &boot_alloc, bootinfo);
 
     memory_initialize_array(&boot_alloc, bootinfo);
 
     /* After this, VGA output is not possible until we switch to the
-     * new address space (see the call to vm_switch_addr_space() below).
+     * new address space (see the call to pmap_switch_addr_space() below).
      * Attempting it will cause a kernel panic due to a page fault (and the
      * panic handler itself attempts to log). */
     remap_text_video_memory(&boot_alloc);
 
     /* switch to new address space */
-    vm_switch_addr_space(addr_space);
+    pmap_switch_addr_space(addr_space);
 
     enable_global_pages();
 
@@ -387,9 +385,9 @@ void machine_init(const config_t *config) {
      * because the slab allocator needs to allocate a slab to allocate the new
      * slab cache on the slab cache cache.
      *
-     * This must be done before the first time vm_create_addr_space() is called. */
+     * This must be done before the first time pmap_create_addr_space() is called. */
     if(pae_enabled) {
-        vm_pae_create_pdpt_cache();
+        pae_create_pdpt_cache();
     }
 
     /* choose a system call implementation */

kernel/interface/i686/trap.asm
@@ -263,7 +263,7 @@ fast_amd_entry:
     mov gs, dx                          ; load gs with per-cpu data segment selector
     mov esp, [gs:GDT_LENGTH * 8 + 4]    ; load kernel stack pointer from TSS
                                         ; Stack pointer is at offset 4 in the TSS, and
-                                        ; the TSS follows the GDT (see cpu_data_t).
+                                        ; the TSS follows the GDT (see percpu_t).
     
     ; For details on the stack layout, see comments in interrupt_entry above and
     ; the definition of the trapframe_t type.

include/kernel/infrastructure/i686/percpu.h
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2019-2024 Philippe Aubertin.
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the author nor the names of other contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef JINUE_KERNEL_INFRASTRUCTURE_I686_PERCPU_H
+#define JINUE_KERNEL_INFRASTRUCTURE_I686_PERCPU_H
+
+#include <kernel/infrastructure/i686/isa/regs.h>
+#include <kernel/infrastructure/i686/types.h>
+
+#define PERCPU_DATA_ALIGNMENT 256
+
+static inline percpu_t *get_percpu_data(void) {
+    /* CPU data structure is at address zero within the per-cpu data segment */
+    percpu_t *const zero = (percpu_t *)0;
+    return (percpu_t *)get_gs_ptr( (uint32_t *)&zero->self );
+}
+
+static inline tss_t *get_tss(void) {
+    return &get_percpu_data()->tss;
+}
+
+static inline addr_space_t *get_current_addr_space(void) {
+    return get_percpu_data()->current_addr_space;
+}
+
+void init_percpu_data(percpu_t *data);
+
+#endif

commit ae359e831902939a00026b59f7a73f1cf8773e46

Fix kernel pointer offset used by SYSCALL instruction (#90)
This PR fixes an issue introduced by #83 where the trap code for system
calls that use the `SYSCALL` instruction do not use the correct offset
in the per-CPU data structure when it retrieves the kernel stack
pointer. This was caused by a change to the `percpu_t` structure that
wasn't reflected in the offset the assembly language code uses.

include/kernel/infrastructure/i686/asm/descriptors.h
@@ -64,7 +64,7 @@
 #define GDT_USER_TLS_DATA        7
 
 /** number of descriptors in GDT */
-#define GDT_LENGTH               8
+#define GDT_NUM_ENTRIES          8
 
 /** offset of descriptor type in descriptor */
 #define SEG_FLAGS_OFFSET            40

include/kernel/infrastructure/i686/asm/percpu.h
@@ -1,0 +1,45 @@
+/*
+ * Copyright (C) 2024 Philippe Aubertin.
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the author nor the names of other contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef JINUE_KERNEL_INFRASTRUCTURE_I686_ASM_PERCPU_H
+#define JINUE_KERNEL_INFRASTRUCTURE_I686_ASM_PERCPU_H
+
+#include <kernel/infrastructure/i686/asm/descriptors.h>
+
+/* The PERCPU_OFFSET_... definitions here must match the offsets in the
+ * percpu_t struct. They are used by assembly language code that can't use the
+ * struct definition. */
+
+#define PERCPU_OFFSET_GDT  8
+
+#define PERCPU_OFFSET_TSS  (PERCPU_OFFSET_GDT + 8 * GDT_NUM_ENTRIES)
+
+#endif

include/kernel/infrastructure/i686/asm/tss.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2024 Philippe Aubertin.
+ * All rights reserved.
+
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the author nor the names of other contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef JINUE_KERNEL_INFRASTRUCTURE_I686_ASM_TSS_H
+#define JINUE_KERNEL_INFRASTRUCTURE_I686_ASM_TSS_H
+
+#define TSS_OFFSET_ESP0 4
+
+#endif

include/kernel/infrastructure/i686/types.h
@@ -111,12 +111,11 @@ typedef struct {
 } tss_t;
 
 struct percpu_t {
+    /* Assembly language code accesses members in this structure. Make sure to
+     * update the PERCPU_OFFSET_... definitions when you change its layout. */
     struct percpu_t     *self;
     addr_space_t        *current_addr_space;
-    seg_descriptor_t     gdt[GDT_LENGTH];
-    /* The assembly-language system call entry point for the SYSCALL instruction
-     * (fast_amd_entry in trap.asm) makes assumptions regarding the location of
-     * the TSS within this structure. */
+    seg_descriptor_t     gdt[GDT_NUM_ENTRIES];
     tss_t                tss;
 };
 

kernel/infrastructure/i686/init.c
@@ -164,7 +164,7 @@ static void load_selectors(percpu_t *cpu_data, boot_alloc_t *boot_alloc) {
 
     /* load new GDT and TSS */
     pseudo->addr    = (addr_t)&cpu_data->gdt;
-    pseudo->limit   = GDT_LENGTH * 8 - 1;
+    pseudo->limit   = GDT_NUM_ENTRIES * 8 - 1;
 
     lgdt(pseudo);
 

kernel/interface/i686/trap.asm
@@ -28,6 +28,8 @@
 ; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <kernel/infrastructure/i686/asm/descriptors.h>
+#include <kernel/infrastructure/i686/asm/percpu.h>
+#include <kernel/infrastructure/i686/asm/tss.h>
 #include <kernel/interface/i686/asm/irq.h>
 #include <kernel/machine/asm/machine.h>
 
@@ -261,9 +263,9 @@ fast_amd_entry:
     ; instruction.
     mov edx, SEG_SELECTOR(GDT_PER_CPU_DATA, RPL_KERNEL)
     mov gs, dx                          ; load gs with per-cpu data segment selector
-    mov esp, [gs:GDT_LENGTH * 8 + 4]    ; load kernel stack pointer from TSS
-                                        ; Stack pointer is at offset 4 in the TSS, and
-                                        ; the TSS follows the GDT (see percpu_t).
+    
+    ; load kernel stack pointer from TSS
+    mov esp, [gs:PERCPU_OFFSET_TSS + TSS_OFFSET_ESP0]
     
     ; For details on the stack layout, see comments in interrupt_entry above and
     ; the definition of the trapframe_t type.