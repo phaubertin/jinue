e46e945e854b4b0b6b4164c0edfec00be25d8076
Philippe Aubertin <philippe.aubertin@gmail.com>
2025-01-06 19:25:48 -0500

    Split init_percpu_data()

--- kernel/infrastructure/i686/percpu.c
+++ kernel/infrastructure/i686/percpu.c
@@ -34,53 +34,9 @@
 #include <kernel/machine/tls.h>
 #include <string.h>
 
-static void set_tls_segment(percpu_t *data, void *addr, size_t size) {
-    data->gdt[GDT_USER_TLS_DATA] = SEG_DESCRIPTOR(
-        addr,
-        size - 1,
-        SEG_TYPE_DATA | SEG_FLAG_USER | SEG_FLAG_NORMAL
-    );
-}
+static void initialize_tss(percpu_t *percpu) {
+    tss_t *tss = &percpu->tss;
 
-void machine_set_thread_local_storage(const thread_t *thread) {
-    percpu_t *data = get_percpu_data();
-    set_tls_segment(data, thread->local_storage_addr, thread->local_storage_size);
-}
-
-void init_percpu_data(percpu_t *data) {
-    tss_t *tss;
-    
-    tss = &data->tss;
-    
-    /* initialize with zeroes  */
-    memset(data, '\0', sizeof(percpu_t));
-    
-    data->self                      = data;
-    data->current_addr_space        = NULL;
-    
-    /* initialize GDT */
-    data->gdt[GDT_NULL] = SEG_DESCRIPTOR(0, 0, 0);
-    
-    data->gdt[GDT_KERNEL_CODE] =
-        SEG_DESCRIPTOR( 0,      0xfffff,                SEG_TYPE_CODE  | SEG_FLAG_KERNEL | SEG_FLAG_NORMAL);
-    
-    data->gdt[GDT_KERNEL_DATA] =
-        SEG_DESCRIPTOR( 0,      0xfffff,                SEG_TYPE_DATA  | SEG_FLAG_KERNEL | SEG_FLAG_NORMAL);
-    
-    data->gdt[GDT_USER_CODE] =
-        SEG_DESCRIPTOR( 0,      0xfffff,                SEG_TYPE_CODE  | SEG_FLAG_USER   | SEG_FLAG_NORMAL);
-    
-    data->gdt[GDT_USER_DATA] =
-        SEG_DESCRIPTOR( 0,      0xfffff,                SEG_TYPE_DATA  | SEG_FLAG_USER   | SEG_FLAG_NORMAL);
-    
-    data->gdt[GDT_TSS] =
-        SEG_DESCRIPTOR( tss,    TSS_LIMIT-1,            SEG_TYPE_TSS   | SEG_FLAG_KERNEL | SEG_FLAG_TSS);
-    
-    data->gdt[GDT_PER_CPU_DATA] =
-        SEG_DESCRIPTOR( data,   sizeof(percpu_t)-1,   SEG_TYPE_DATA  | SEG_FLAG_KERNEL | SEG_FLAG_32BIT | SEG_FLAG_IN_BYTES | SEG_FLAG_NOSYSTEM | SEG_FLAG_PRESENT);
-    
-    set_tls_segment(data, NULL, 0);
-    
     /* setup kernel stack in TSS */
     tss->ss0  = SEG_SELECTOR(GDT_KERNEL_DATA, RPL_KERNEL);
     tss->ss1  = SEG_SELECTOR(GDT_KERNEL_DATA, RPL_KERNEL);
@@ -98,4 +54,67 @@ void init_percpu_data(percpu_t *data) {
      *   segment limit, there is no I/O permission map, and all I/O instructions
      *   generate exceptions when the CPL is greater than the current IOPL. " */
     tss->iomap = TSS_LIMIT;
+}
+
+static void set_normal_segment(percpu_t *percpu, int index, int type) {
+    percpu->gdt[index] = SEG_DESCRIPTOR(
+        0,
+        0xfffff,
+        type | SEG_FLAG_NORMAL
+    );
+}
+
+static void set_tls_segment(percpu_t *percpu, void *addr, size_t size) {
+    percpu->gdt[GDT_USER_TLS_DATA] = SEG_DESCRIPTOR(
+        addr,
+        size - 1,
+        SEG_TYPE_DATA | SEG_FLAG_USER | SEG_FLAG_NORMAL
+    );
+}
+
+static void initialize_gdt(percpu_t *percpu) {
+    percpu->gdt[GDT_NULL] = SEG_DESCRIPTOR(0, 0, 0);
+
+    set_normal_segment(percpu, GDT_KERNEL_CODE, SEG_TYPE_CODE | SEG_FLAG_KERNEL);
+    
+    set_normal_segment(percpu, GDT_KERNEL_DATA, SEG_TYPE_DATA | SEG_FLAG_KERNEL);
+    
+    set_normal_segment(percpu, GDT_USER_CODE, SEG_TYPE_CODE | SEG_FLAG_USER);
+    
+    set_normal_segment(percpu, GDT_USER_DATA, SEG_TYPE_DATA | SEG_FLAG_USER);
+    
+    percpu->gdt[GDT_TSS] = SEG_DESCRIPTOR(
+        &percpu->tss,
+        TSS_LIMIT - 1,
+        SEG_TYPE_TSS | SEG_FLAG_KERNEL | SEG_FLAG_TSS
+    );
+    
+    percpu->gdt[GDT_PER_CPU_DATA] = SEG_DESCRIPTOR(
+        percpu,
+        sizeof(percpu_t)-1,
+          SEG_TYPE_DATA
+        | SEG_FLAG_KERNEL
+        | SEG_FLAG_32BIT
+        | SEG_FLAG_IN_BYTES
+        | SEG_FLAG_NOSYSTEM
+        | SEG_FLAG_PRESENT
+    );
+    
+    set_tls_segment(percpu, NULL, 0);
+}
+
+void init_percpu_data(percpu_t *percpu) {
+    /* initialize with zeroes  */
+    memset(percpu, '\0', sizeof(percpu_t));
+    
+    percpu->self                = percpu;
+    percpu->current_addr_space  = NULL;
+
+    initialize_tss(percpu);
+    initialize_gdt(percpu);
+}
+
+void machine_set_thread_local_storage(const thread_t *thread) {
+    percpu_t *percpu = get_percpu_data();
+    set_tls_segment(percpu, thread->local_storage_addr, thread->local_storage_size);
 }
