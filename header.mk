bfb72ac5bd1ef531b5fd2f3f5c3c007dd11ba4dd
Philippe Aubertin <39178965+phaubertin@users.noreply.github.com>
2025-12-08 20:09:26 -0500

    Configure architecture flags through the configure script (#137)
    * Have the `configure` script parse command line arguments and generate
    an include file for the Makefile (`config.gen.mk`).
    * Pass the architecture-specific CFLAGS and LDFLAGS through that file
    (`--arch` argument).
    * Add some additional command line arguments to the `configure' script
    to cover current use cases ('--cc', '--debug', '--werror').
    * Adapt the GitHub Actions workflow to make use of these new arguments.
    
    This is preparation work for x86_64 support.

--- header.mk
+++ header.mk
@@ -53,6 +53,14 @@
 # Makefiles that include this file must define this.
 jinue_root          ?= .
 
+config				= $(jinue_root)/config.gen.mk
+
+ifeq (,$(wildcard $(config)))
+$(error Source code copy is not configured: please run ./configure in the top directory before running make.)
+endif
+
+include $(config)
+
 # project directory structure
 devel                = $(jinue_root)/devel
 includes             = $(jinue_root)/include
@@ -98,11 +106,7 @@ unclean              = $(unclean.build) $(unclean.extra)
 unclean_recursive   ?=
 
 # debug/release build
-#
-# Anything other than DEBUG=yes means release
-DEBUG   ?= yes
-
-ifeq ($(DEBUG), yes)
+ifeq ($(CONFIG_DEBUG), 1)
 	CPPFLAGS.debug		 =
 	CFLAGS.debug 		 = -g3
 	NASMFLAGS.debug		 =
@@ -116,19 +120,20 @@ endif
 #
 # These flags are used when preprocessing C and assembly language files.
 CPPFLAGS.includes    = -I$(includes)
-CPPFLAGS.arch        = -m32 -march=pentium -mtune=generic
+CPPFLAGS.arch        = $(CONFIG_CFLAGS_ARCH)
 CPPFLAGS.others      = -nostdinc
 CPPFLAGS             = $(CPPFLAGS.arch) $(CPPFLAGS.includes) $(CPPFLAGS.debug) $(CPPFLAGS.others) $(CPPFLAGS.extra)
 
 # C compiler flags
-CFLAGS.warnings      = -std=c99 -pedantic -Wall -Wno-array-bounds -Werror=implicit -Werror=uninitialized -Werror=return-type
-CFLAGS.arch          = -m32 -march=pentium -mtune=generic
+CC					 = $(CONFIG_CC)
+CFLAGS.warnings      = -std=c99 -pedantic -Wall -Wno-array-bounds -Werror=implicit -Werror=uninitialized -Werror=return-type $(CONFIG_WERROR)
+CFLAGS.arch          = $(CONFIG_CFLAGS_ARCH)
 CFLAGS.optimization  = -O3
 CFLAGS.others        = -ffreestanding -fno-pie -fno-common -fno-omit-frame-pointer -fno-delete-null-pointer-checks -fno-asynchronous-unwind-tables
 CFLAGS               = $(CFLAGS.arch) $(CFLAGS.optimization) $(CFLAGS.debug) $(CFLAGS.warnings) $(CFLAGS.others) $(CFLAGS.extra)
 
 # Linker flags
-LDFLAGS.arch         = -Wl,-m,elf_i386
+LDFLAGS.arch         = $(CONFIG_LDFLAGS_ARCH)
 LDFLAGS.others       = -static -nostdlib -Wl,-z,noexecstack
 LDFLAGS              = $(LDFLAGS.arch) $(LDFLAGS.others) $(LDFLAGS.extra)
 
