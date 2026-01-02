a272e16b843988a62a6d987314a7a3c8166483ed
Philippe Aubertin <philippe.aubertin@gmail.com>
2026-01-02 01:16:41 -0500

    Copyright year 2026

--- include/kernel/infrastructure/i686/cpuinfo.h
+++ include/kernel/infrastructure/i686/cpuinfo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2025 Philippe Aubertin.
+ * Copyright (C) 2019-2026 Philippe Aubertin.
  * All rights reserved.
 
  * Redistribution and use in source and binary forms, with or without
